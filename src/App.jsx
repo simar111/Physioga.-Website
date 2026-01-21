73d8f91a8ce2ee96e66f0f49fdb29aad2304bd42
simar111 <simarnarula1477@gmail.com>
2026-01-21 23:28:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold">
+        React + Vite + Tailwind CSS
+      </h1>
+    </div>
+  )
+}
